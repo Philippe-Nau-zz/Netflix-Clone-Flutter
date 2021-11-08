5bea30760da24acc25be2ea3bcc05a2b0504ad4a
Philippe-Nau <philippe.bnu@gmail.com>
2021-11-08 10:40:32 -0300

    init project

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
